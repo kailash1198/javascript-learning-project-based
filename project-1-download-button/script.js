e905d86e23405328c94b4da2899ff17ca7803594
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-10-22 15:25:20 +0000

    js basics line of coding

---
+++ project-1-download-button/script.js
@@ -0,0 +1,8 @@
+// console.log("hello world");
+
+function downloadFun() {
+    let iframe = document.getElementById('download');
+    iframe.src = ""
+}
+
+let downloadButton = document.getElementById('download');
