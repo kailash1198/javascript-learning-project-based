ac1e22f35d4ee1fa2382d9c2d421caff68401ccf
kailash kumar <kailash414243@gmail.com>
2023-01-27 22:59:55 +0530

    loading card done

---
+++ animated-loading-card/script.js
@@ -0,0 +1,8 @@
+let myBtn = document.getElementById('btn');
+let myCard = document.getElementById('card');
+
+
+myCard.style.display = 'none';
+window.addEventListener('load', function() {
+    myCard.style.display = 'block';
+})
