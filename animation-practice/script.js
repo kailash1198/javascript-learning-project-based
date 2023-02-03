63952be1f26f89618eaf3714d65caa8149023402
kailash kumar <kailash414243@gmail.com>
2023-02-03 23:11:18 +0530

    animated card loading using javascript dom

---
+++ animation-practice/script.js
@@ -0,0 +1,13 @@
+let myCard = document.getElementById('my-card');
+let myCard2 = document.getElementById('my-card-2');
+let myCard3 = document.getElementById('my-card-3');
+myCard.style.display = 'none';
+myCard2.style.display = 'none';
+myCard3.style.display = 'none';
+
+window.addEventListener('load', setTimeout(function() {
+    myCard.style.display = 'block';
+    myCard2.style.display = 'block';
+    myCard3.style.display = 'block';
+
+}, 2000))
