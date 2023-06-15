a0acb3ed25596dd344e1fadf4f9bdb2877f3f092
kailash kumar <kailash414243@gmail.com>
2023-06-15 23:53:23 +0530

    color flipper project

---
+++ color-flipper/script.js
@@ -0,0 +1,8 @@
+let changeBtn = document.getElementById('click-btn');
+
+let changecolorBox = document.getElementById('change-box');
+
+changeBtn.addEventListener('click', function(){
+    // changecolorBox.style.background = 'green';
+    changecolorBox.style.background = 'blue';
+})
